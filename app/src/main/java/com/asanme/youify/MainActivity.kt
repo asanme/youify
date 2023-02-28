c9340154a830f3619f55259b8d03fad4ea53c68a
asanme <u.alexsanme@gmail.com>
2023-02-28 10:56:35 +0100

    Fixed commit

--- app/src/main/java/com/asanme/youify/MainActivity.kt
+++ app/src/main/java/com/asanme/youify/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.asanme.youify
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.asanme.youify.model.auth.AuthController
 import com.asanme.youify.ui.theme.YouifyTheme
-import com.asanme.youify.view.MainView
-import com.asanme.youify.view.SignInView
+import net.openid.appauth.AuthorizationService
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,5 +28,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun App() {
-    SignInView()
+    val context = LocalContext.current
+    val authRequest = AuthController(
+        context.resources.openRawResource(R.raw.auth_config).bufferedReader().use { it.readText() }
+    ).getAuthorizationRequest()
+
+    val authService = AuthorizationService(context)
+    val authIntent: Intent = authService.getAuthorizationRequestIntent(authRequest)
+
+    val pendingIntent = PendingIntent.getActivity(
+        context,
+        1,
+        authIntent,
+        PendingIntent.FLAG_IMMUTABLE
+    ).send()
 }
--- app/src/main/java/com/asanme/youify/model/auth/AuthController.kt
+++ app/src/main/java/com/asanme/youify/model/auth/AuthController.kt
@@ -1,4 +1,34 @@
 package com.asanme.youify.model.auth
 
-class AuthController {
+import androidx.core.net.toUri
+import net.openid.appauth.AuthorizationRequest
+import net.openid.appauth.AuthorizationServiceConfiguration
+import net.openid.appauth.AuthorizationServiceDiscovery
+import net.openid.appauth.ResponseTypeValues
+import org.json.JSONObject
+
+
+class AuthController(
+    private val auth_config: String
+) {
+    private val MY_REDIRECT_URI =
+        "com.asanme.youify:/oauth2redirect"
+
+    private val MY_CLIENT_ID: String =
+        "629936952678-lbq4hkcn2p14r38844pa65d21rspuaie.apps.googleusercontent.com"
+
+    fun getAuthorizationRequest(): AuthorizationRequest {
+        val serviceConfig = AuthorizationServiceConfiguration(
+            AuthorizationServiceDiscovery(
+                JSONObject(auth_config)
+            )
+        )
+
+        return AuthorizationRequest.Builder(
+            serviceConfig,
+            MY_CLIENT_ID,
+            ResponseTypeValues.CODE,
+            MY_REDIRECT_URI.toUri()
+        ).setScope("https://www.googleapis.com/auth/youtube").build()
+    }
 }
